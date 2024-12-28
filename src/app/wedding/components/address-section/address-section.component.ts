1d9adfc7d1b7aae36a862b9ddc3dbf12ab223141
gamiopatricio <gamiopatricio@gmail.com>
2024-12-28 12:48:33 -0500

    Music Player

--- src/app/wedding/components/address-section/address-section.component.ts
+++ src/app/wedding/components/address-section/address-section.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./address-section.component.scss']
 })
 export class AddressSectionComponent {
+  ceremonyLocation: string = 'https://maps.app.goo.gl/BFhSpoWHNYxFLpF67';
+  receptionLocation: string = 'https://maps.app.goo.gl/8YGadjBnQ4EqKGvQA';
 
+  // Método para abrir el mapa en una nueva ventana
+  openMap(location: string): void {
+    window.open(location, '_blank');
+  }
 }
