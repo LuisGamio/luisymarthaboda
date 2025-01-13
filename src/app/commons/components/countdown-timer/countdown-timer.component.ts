e58417a6aac81c96511d733c0519534889092a90
gamiopatricio <gamiopatricio@gmail.com>
2025-01-12 20:09:32 -0500

    123

--- src/app/commons/components/countdown-timer/countdown-timer.component.ts
+++ src/app/commons/components/countdown-timer/countdown-timer.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countdown-timer.component.scss']
 })
 export class CountdownTimerComponent implements OnInit {
+  // weddingDate: Date = new Date("2025-02-22T14:00:00-05:00"); // Fecha de la boda
+  // timeRemaining: number = 0;
+  // days: number = 0;
+  // hours: number = 0;
+  // minutes: number = 0;
+  // seconds: number = 0;
+
+  // ngOnInit() {
+  //   this.updateCountdown();
+  //   setInterval(() => this.updateCountdown(), 1000);
+  // }
+
+  // updateCountdown() {
+  //   const now = new Date();
+  //   this.timeRemaining = this.weddingDate.getTime() - now.getTime();
+
+  //   if (this.timeRemaining > 0) {
+  //     this.days = Math.floor(this.timeRemaining / (1000 * 60 * 60 * 24));
+  //     this.hours = Math.floor((this.timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //     this.minutes = Math.floor((this.timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  //     this.seconds = Math.floor((this.timeRemaining % (1000 * 60)) / 1000);
+  //   }
+  // }
+
+
   weddingDate: Date = new Date("2025-02-22T14:00:00-05:00"); // Fecha de la boda
   timeRemaining: number = 0;
   days: number = 0;
@@ -14,8 +39,18 @@ export class CountdownTimerComponent implements OnInit {
   seconds: number = 0;
 
   ngOnInit() {
-    this.updateCountdown();
-    setInterval(() => this.updateCountdown(), 1000);
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    const tick = () => {
+      this.updateCountdown();
+      if (this.timeRemaining > 0) {
+        const nextTick = 1000 - (new Date().getTime() % 1000); // Sincroniza con el siguiente segundo exacto
+        setTimeout(tick, nextTick);
+      }
+    };
+    tick();
   }
 
   updateCountdown() {
@@ -27,6 +62,8 @@ export class CountdownTimerComponent implements OnInit {
       this.hours = Math.floor((this.timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       this.minutes = Math.floor((this.timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
       this.seconds = Math.floor((this.timeRemaining % (1000 * 60)) / 1000);
+    } else {
+      this.days = this.hours = this.minutes = this.seconds = 0;
     }
   }
 }
